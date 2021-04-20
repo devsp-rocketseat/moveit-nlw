654551acb123cf5d06b6a3054b77fd6ef8216694
DevSP <contatodevsp@gmail.com>
2021-04-19 23:16:10 -0300

    Carregando um novo desafio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,15 +1,23 @@
 import { createContext, ReactNode, useState } from 'react'
+import challenges from '../../challenges.json'
 
-interface ChallengesPrividerProps {
-  children: ReactNode
+interface Challenge {
+  type: 'body' | 'eye'
+  description: string
+  amount: number
 }
 
 interface ChallengesContextData {
   level: number
   currentExperience: number
   challengesCompleted: number
+  activeChallenge: Challenge
   levelUp: () => void
   startNewChallenge: () => void
+}
+
+interface ChallengesPrividerProps {
+  children: ReactNode
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData)
@@ -19,12 +27,17 @@ export function ChallengesPrivider({ children }: ChallengesPrividerProps) {
   const [currentExperience, setCurrentExperience] = useState(0)
   const [challengesCompleted, setChallengesCompleted] = useState(0)
 
+  const [activeChallenge, setActiveChallenge] = useState(null)
+
   function levelUp() {
     setLevel(level + 1)
   }
 
   function startNewChallenge() {
-    console.log('New challenge')
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+    const challenge = challenges[randomChallengeIndex]
+
+    setActiveChallenge(challenge)
   }
 
   return (
@@ -33,6 +46,8 @@ export function ChallengesPrivider({ children }: ChallengesPrividerProps) {
         level,
         currentExperience,
         challengesCompleted,
+        activeChallenge,
+
         levelUp,
         startNewChallenge,
       }}
